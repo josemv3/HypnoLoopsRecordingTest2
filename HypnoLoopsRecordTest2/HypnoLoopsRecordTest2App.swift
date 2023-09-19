aa26aed543633f48941d9e9d5dbe72f50849e258
Joey Rubin <josemv3@mac.com>
2023-09-19 10:53:01 -0700

    Initial Commit

---
+++ HypnoLoopsRecordTest2/HypnoLoopsRecordTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  HypnoLoopsRecordTest2App.swift
+//  HypnoLoopsRecordTest2
+//
+//  Created by Joey Rubin on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HypnoLoopsRecordTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
